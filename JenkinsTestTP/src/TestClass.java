11f85f60d6fba05b59485842fa39a4155f0839a7
adli <adli@adliSanae>
2021-04-04 22:48:14 +0100

    SECOND COMMIT

--- JenkinsTestTP/src/TestClass.java
+++ JenkinsTestTP/src/TestClass.java
@@ -1,6 +1,6 @@
 
 public class TestClass {
 	public static void main(String args[]){  
-	     System.out.println("Hello Java");  
+	     System.out.println("Hello Java ans all the world");  
 	    }  
 }
